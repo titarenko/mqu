005beb7905f2ff809222acf577c4aecbffd62f4a
Max Nowack <max@unsou.de>
2018-11-16 09:57:17 +0100

    Fix tls connections

--- src/connection.js
+++ src/connection.js
@@ -2,6 +2,7 @@ const bramqp = require('bramqp')
 const url = require('url')
 const { EventEmitter } = require('events')
 const { createConnection } = require('net')
+const { connect: createSecureConnection } = require('tls')
 
 const specification = 'rabbitmq/full/amqp0-9-1.stripped.extended'
 
@@ -22,8 +23,9 @@ class Connection extends EventEmitter {
 
   getHandle () {
     return this.handlePromise || (this.handlePromise = new Promise((resolve, reject) => {
-      const { host, port } = this.options
-      const socket = createConnection(port, host)
+      const socket = this.options.tls
+        ? createSecureConnection(this.options)
+        : createConnection(this.options)
       socket.on('error', error => this.emit('error', error))
       bramqp.initialize(socket, specification, (error, handle) => {
         if (error) {
@@ -66,14 +68,16 @@ class Connection extends EventEmitter {
   }
 
   parseConnectionString (connectionString = '') {
-    const { auth, hostname, port, pathname } = url.parse(connectionString)
+    const { auth, hostname, port, pathname, protocol } = url.parse(connectionString)
     const [user = 'guest', password = 'guest'] = auth ? auth.split(':') : []
     return {
       user,
       password,
+      servername: hostname || 'localhost',
       host: hostname || 'localhost',
       port: port || '5672',
       vhost: pathname ? pathname.slice(1) : '/',
+      tls: protocol === 'amqps:',
     }
   }
 }
